6afa8d8128f708f710586b102e2ae052ea93209a
Matthias Schrempf <mail@makarl.de>
2023-04-11 17:05:57 +0200

    `PeoplePlugin` added

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_startup_system(setup)
-        .add_system(print_names)
-        .add_system(persons_with_jobs)
+        .add_plugin(PeoplePlugin)
+        .add_plugins(DefaultPlugins)
         .run();
 }
 
@@ -26,12 +25,27 @@ fn setup(mut commands: Commands) {
     });
 }
 
+struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup)
+            .add_system(print_names)
+            .add_system(persons_with_jobs)
+            .add_system(persons_and_jobs);
+    }
+}
+
 fn print_names(person_query: Query<&Person>) {
     person_query.for_each(|person| println!("Hello {}", person.name));
 }
 
 fn persons_with_jobs(person_query: Query<&Person, With<Employed>>) {
     person_query.for_each(|person| println!("Hello {}, you have a job :-)", person.name));
+}
+
+fn persons_and_jobs(q: Query<(&Person, &Employed)>) {
+    q.for_each(|(p, e)| println!("{} works as a {:?}", p.name, e.job));
 }
 
 #[derive(Component)]
