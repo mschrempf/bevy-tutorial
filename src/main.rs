1f066f0f017d414821f319b1cd344dea3dee763f
Matthias Schrempf <mail@makarl.de>
2023-04-11 16:43:01 +0200

    `print_names` system with `Person` component added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(setup)
+        .add_system(print_names)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Person {
+        name: "Matthias".to_string(),
+    });
+    commands.spawn(Person {
+        name: "Katja".to_string(),
+    });
+}
+
+fn print_names(person_query: Query<&Person>) {
+    person_query.for_each(|person| println!("Hello {}", person.name));
+}
+
+#[derive(Component)]
+struct Person {
+    name: String,
 }
